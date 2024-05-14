0c15d28a11b65825785382b9ae5ce44d38a853a0
gsallum <gabrielsallum2006@gmail.com>
2024-05-13 22:04:27 -0300

    foto fundo

--- cobrinha.py
+++ cobrinha.py
@@ -8,3 +8,8 @@ largura, altura = 1300, 700
 tela = pygame.display.set_mode((largura, altura))
 telafim = pygame.display.set_mode((largura, altura))
 relogio = pygame.time.Clock()
+
+#foto fundo
+fundo = pygame.image.load('acad.png').convert_alpha()
+fundo.set_alpha(190)
+fundo = pygame.transform.scale(fundo, (largura, altura))
