19e3248fe6330d17c2bb33d815bdf06c3da2aa10
gsallum <gabrielsallum2006@gmail.com>
2024-05-13 22:01:52 -0300

    iniciais cobrinha

---
+++ cobrinha.py
@@ -0,0 +1,10 @@
+import pygame
+import random
+
+
+pygame.init()
+pygame.display.set_caption("ACADEMIA PYTHON COBRINHA")
+largura, altura = 1300, 700
+tela = pygame.display.set_mode((largura, altura))
+telafim = pygame.display.set_mode((largura, altura))
+relogio = pygame.time.Clock()
