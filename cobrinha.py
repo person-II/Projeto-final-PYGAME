b194edffceb0c35d95fceda9d65a15dd0a1a7249
gsallum <gabrielsallum2006@gmail.com>
2024-05-14 22:21:07 -0300

    teclas e grid

--- cobrinha.py
+++ cobrinha.py
@@ -49,3 +49,40 @@ def desenhar_pontuacao(pontuacao):
     fonte = pygame.font.SysFont("Verdana", 35)
     texto = fonte.render(f"Pontos: {pontuacao}x", True, vermelha)
     tela.blit(texto, [1, 1])
+
+def selecionar_velocidade(tecla, velocidade_x, velocidade_y):
+    if (tecla == pygame.K_s or tecla == pygame.K_DOWN) and velocidade_y != -tamanho_quadrado_cobra:
+        velocidade_x = 0
+        velocidade_y = tamanho_quadrado_cobra
+    
+    elif (tecla == pygame.K_w or tecla == pygame.K_UP) and velocidade_y != tamanho_quadrado_cobra:
+        velocidade_x = 0
+        velocidade_y = -tamanho_quadrado_cobra
+
+    elif (tecla == pygame.K_d or tecla == pygame.K_RIGHT) and velocidade_x != -tamanho_quadrado_cobra:
+        velocidade_x = tamanho_quadrado_cobra
+        velocidade_y = 0
+
+    elif (tecla == pygame.K_a or tecla == pygame.K_LEFT)  and velocidade_x != tamanho_quadrado_cobra:
+        velocidade_x = -tamanho_quadrado_cobra
+        velocidade_y = 0
+
+    return velocidade_x, velocidade_y
+
+def desenhar_grid():
+    
+    cor_clara = (50, 50, 50)  # Cor mais clara para o grid
+    cor_escura = (30, 30, 30)  # Cor mais escura para o grid
+    for i in range(0, largura, tamanho_quadrado_cobra):
+        for j in range(0, altura, tamanho_quadrado_cobra):
+            if (i // tamanho_quadrado_cobra) % 2 == 0:
+                if (j // tamanho_quadrado_cobra) % 2 == 0:
+                    cor = cor_clara
+                else:
+                    cor = cor_escura
+            else:
+                if (j // tamanho_quadrado_cobra) % 2 == 0:
+                    cor = cor_escura
+                else:
+                    cor = cor_clara
+            pygame.draw.rect(tela, cor, pygame.Rect(i, j, tamanho_quadrado_cobra, tamanho_quadrado_cobra))
