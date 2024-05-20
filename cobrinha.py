68b74e886fd402b3d686421c98144ba152815b2d
person-II <enzols1@al.insper.edu.br>
2024-05-20 20:13:50 -0300

    vel cobra

--- cobrinha.py
+++ cobrinha.py
@@ -158,15 +158,13 @@ def rodar_jogo():
         # desenhar_pontos
         desenhar_pontuacao(tamanho_cobra - 1)
 
-
-
         # atualizacao da tela
         pygame.display.update()
 
         #nova comida
         if x == comida_x and y == comida_y:
             tamanho_cobra += 1
-            veloc += 1 
+            veloc += 0.5 
             comida_x, comida_y = gerar_comida()
 
         relogio.tick(veloc)
